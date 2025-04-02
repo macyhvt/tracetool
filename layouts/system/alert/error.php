03ba1c521a333b322d4c9a1ad2133e9ed8f939ce
macyhvt <mayankkhatri90@gmail.com>
2025-04-02 13:15:29 +0200

    Major update

--- layouts/system/alert/error.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* no direct script access */
-defined ('_FTK_APP_') OR die('403 FORBIDDEN'); ?>
-<?php require __DIR__ . '/danger.php';
-return;
--- layouts/system/element/drawingnumber.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Register required libraries.
-use Nematrack\Helper\LayoutHelper;
-
-/* no direct script access */
-defined ('_FTK_APP_') OR die('403 FORBIDDEN');
-
-echo LayoutHelper::render(sprintf('system.element.drawingnumber.%s', $this->get('layout', 'default')), $this->data, ['language' => $this->get('language')]);
